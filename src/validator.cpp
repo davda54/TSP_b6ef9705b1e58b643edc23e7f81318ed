fb258530c3c7903f0b6cff44309e8b013be81a70
davda <david.samuel@seznam.cz>
2018-10-11 21:51:53 +0200

    added validator skeleton

--- src/validator.cpp
+++ src/validator.cpp
@@ -1,5 +1,12 @@
-//
-// Created by tomiinek on 11.10.18.
-//
-
 #include "validator.h"
+
+
+int validator::find_best_route(const std::array<int, N>& clusters)
+{
+	return 0;
+}
+
+bool validator::exist_route(const std::array<int, N>& clusters)
+{
+	return false;
+}
