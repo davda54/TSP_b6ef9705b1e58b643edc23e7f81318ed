4d41f6dbe2919c002d4b6e85da493a9627f59fa2
Tomas Nekvinda <tom@neqindi.cz>
2018-10-13 18:09:43 +0200

    Added generator - random, not valid

--- src/generator.h
+++ src/generator.h
@@ -1,5 +1,29 @@
 #ifndef TSP_CHALLENGE_KIWI_GENERATOR_H
 #define TSP_CHALLENGE_KIWI_GENERATOR_H
 
+#include "task.h"
+
+class generator {
+
+public:
+
+    generator(const task& t) : _data(t){}
+
+    std::vector<cluster_id_t> generate_solution() const {
+
+        int n = _data.cluster_count();
+        std::vector<cluster_id_t> solution;
+
+        for (int i=1; i < n; ++i) solution.push_back(i);
+        std::random_shuffle(solution.begin(), solution.end());
+
+        return solution;
+    }
+
+private:
+
+    const task& _data;
+
+};
 
 #endif
