fbe321f3af24006822324d000e0b837c62f95f69
davda <david.samuel@seznam.cz>
2018-10-25 18:22:11 +0200

    input generator added

--- src/generator.cpp
+++ src/generator.cpp
@@ -3,4 +3,4 @@
 using namespace std;
 
 uniform_real_distribution<float> generator::_uniform_dist = uniform_real_distribution<float>(0.0f, 1.0f);
-mt19937 generator::random_engine = mt19937(42/*random_device()()*/);
+mt19937 generator::random_engine = mt19937(random_device()());
--- src/task.h
+++ src/task.h
@@ -100,6 +100,8 @@ private:
     city_id_t _start_city;
 	cluster_id_t _start_cluster;
 
+	void generate_input(size_t cluster_count, size_t city_count, float average_branching, size_t seed);
+
     // TODO: udelej poradne!
     // first axis day, second city-from, third city-to
     std::vector<std::vector<std::vector<cost_t>>> _graph;
