ada231bf435e6711a5bddb6212319603aa241089
Tomas Nekvinda <tom@neqindi.cz>
2018-10-26 10:57:05 +0200

    Config changes

--- src/config.cpp
+++ src/config.cpp
@@ -1,7 +1,28 @@
 #include "config.h"
 
-float config::GREEDY_SEARCH_EXP = 3.8f;
-int config::GREEDY_SEARCH_KNBRS = 2;
+// 2.in .. 3, 1.2 ---> 1628   / 2137      // 1562,   1.5, 0.05
+// 3.in .. 3, 2.5 ---> 44219  / 42148	  // 41061,  3.8, 0.05
+// 4.in .. 3, 2.3 ---> 111212 / 109968	  // 110008,
+
+float config::GREEDY_SEARCH_EXP = 1.2f;
+int config::GREEDY_SEARCH_KNBRS = 3;
 float config::GREEDY_SEARCH_RATIO = 0.05f;
+
+
+/* resources\TSALESMAN2-1.in, INITIAL_TEMP = 10*10, COOLING_TEMP = 0.99999994
+     *
+     * score average: 1396
+     *
+     */
+
+/* resources\TSALESMAN2-3.in, INITIAL_TEMP = 1*150, COOLING_TEMP = 0.9999998
+ *
+ * score average: 39338.8
+ * score std deviation: 776.974
+ * score min: 38265
+ * score max: 40851
+ *
+ */
+
 double config::INITIAL_TEMP = 1;
 double config::COOLING_TEMP = 0.9999998;
--- src/config.h
+++ src/config.h
@@ -5,29 +5,9 @@ class config
 {
 public:
 
-
-    /* resources\TSALESMAN2-1.in, INITIAL_TEMP = 10*10, COOLING_TEMP = 0.99999994
-     *
-     * score average: 1396
-     *
-     */
-
-    /* resources\TSALESMAN2-3.in, INITIAL_TEMP = 1*150, COOLING_TEMP = 0.9999998
-     *
-     * score average: 39338.8
-     * score std deviation: 776.974
-     * score min: 38265
-     * score max: 40851
-     *
-     */
-
-    // ANNEALING PARAMS:
     static double INITIAL_TEMP; //best: 0.3
     static double COOLING_TEMP; // best: 0.9999995
 
-    // 2.in .. 3, 1.2 ---> 1628   / 2137      // 1562,   1.5, 0.05
-    // 3.in .. 3, 2.5 ---> 44219  / 42148	  // 41061,  3.8, 0.05
-    // 4.in .. 3, 2.3 ---> 111212 / 109968	  // 110008,
     static float GREEDY_SEARCH_EXP;
     static int GREEDY_SEARCH_KNBRS;
     static float GREEDY_SEARCH_RATIO; // 1 -> average, 0 -> min
